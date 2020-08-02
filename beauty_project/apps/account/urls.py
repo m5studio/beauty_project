6c1795bf310b9994a38346c1a5ee2aab8822f468
Denis <jquphp@gmail.com>
2020-08-02 16:11:56 +0300

    upd

--- beauty_project/apps/account/urls.py
+++ beauty_project/apps/account/urls.py
@@ -17,10 +17,10 @@ app_name = 'user'
 urlpatterns = [
     path('user/', include([
         path('register/', registration_view, name='registration'),
-        path('login/', login_view, name="login"),
-        path('logout/', logout_view, name="logout"),
+        path('login/', login_view, name='login'),
+        path('logout/', logout_view, name='logout'),
 
-        path('cabinet/', personal_cabinet_view, name="cabinet"),
+        path('cabinet/', personal_cabinet_view, name='cabinet'),
 
         path('register-by-phone/', register_by_phone_view, name='registration-by-phone'),
         path('register-password/', register_password_view, name='registration-password'),
--- beauty_project/config/settings.py
+++ beauty_project/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
 
+    'django_cleanup.apps.CleanupConfig',
     'django_unused_media',
     'imagekit',
     'mptt',
